b6dac8ad61ce9f694f9798b89c2fd9f95c5253a2
Alejandro Aguilar <alejandroaquilar@gmail.com>
2019-05-31 12:49:21 -0700

    changed app color to yellow

--- src/components/App.js
+++ src/components/App.js
@@ -1,128 +1,140 @@
-import React from 'react';
-import '../App.css';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import "typeface-roboto";
 
-import CenteredGrid from './FluidGrid';
+import CenteredGrid from "./FluidGrid";
 
 /* App Bar */
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
-import { fade } from '@material-ui/core/styles/colorManipulator';
-
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles/colorManipulator";
 
 /* Footer */
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-  },
-  main: {
-    marginTop: theme.spacing(5),
-    marginBottom: theme.spacing(5),
-    width: '100%',
-  },
-  footer: {
-    padding: theme.spacing(2),
-    marginTop: 'auto',
-    backgroundColor: 'white',
-  },
+	root: {
+		display: "flex",
+		flexDirection: "column",
+		minHeight: "100vh",
+		backgroundColor: "#fffde7"
+	},
+	main: {
+		marginTop: theme.spacing(5),
+		marginBottom: theme.spacing(5),
+		width: "100%"
+	},
+	footer: {
+		padding: theme.spacing(2),
+		marginTop: "auto",
+		backgroundColor: "#fff9c4"
+	},
 
-  appTitle: {
-    flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
-      },
-    },
-  },
+	appBar: {
+		backgroundImage: "linear-gradient(to right, #fdd835, #ffd600)"
+	},
+
+	appTitle: {
+		flexGrow: 1,
+		[theme.breakpoints.up("sm")]: {
+			display: "block"
+		}
+	},
+	search: {
+		position: "relative",
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		"&:hover": {
+			backgroundColor: fade(theme.palette.common.white, 0.25)
+		},
+		marginLeft: 0,
+		width: "100%",
+		display: "none",
+		[theme.breakpoints.up("sm")]: {
+			marginLeft: theme.spacing(1),
+			width: "auto",
+			display: "block"
+		}
+	},
+	searchIcon: {
+		width: theme.spacing(7),
+		height: "100%",
+		position: "absolute",
+		pointerEvents: "none",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	inputRoot: {
+		color: "inherit"
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 7),
+		transition: theme.transitions.create("width"),
+		width: "100%",
+		[theme.breakpoints.up("sm")]: {
+			width: 120,
+			"&:focus": {
+				width: 200
+			}
+		}
+	}
 }));
 
 function App() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
 
-      <AppBar position="static">
-        <Toolbar>
-          <Typography className={classes.appTitle} variant="h6" noWrap>
-            My Recipes
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
-          </div>
-        </Toolbar>
-      </AppBar>
+			<AppBar className={classes.appBar} position="static">
+				<Toolbar>
+					<Typography
+						className={classes.appTitle}
+						variant="h6"
+						noWrap
+					>
+						Grub Note
+					</Typography>
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
+						</div>
+						<InputBase
+							placeholder="Search…"
+							classes={{
+								root: classes.inputRoot,
+								input: classes.inputInput
+							}}
+						/>
+					</div>
+				</Toolbar>
+			</AppBar>
 
-      <Container component="main" className={classes.main} maxWidth="auto">
-        <CenteredGrid />
-      </Container>
+			<Container
+				component="main"
+				className={classes.main}
+				maxWidth="auto"
+			>
+				<CenteredGrid />
+			</Container>
 
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Typography variant="body1" className="footer-copyright">
-            {' '}
-            © {new Date().getFullYear()} Alejandro Aguilar
-          </Typography>
-        </Container>
-      </footer>
-
-    </div>
-  );
+			<footer className={classes.footer}>
+				<Container maxWidth="sm">
+					<Typography variant="body1" className="footer-copyright">
+						{" "}
+						© {new Date().getFullYear()} Alejandro Aguilar
+					</Typography>
+				</Container>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
